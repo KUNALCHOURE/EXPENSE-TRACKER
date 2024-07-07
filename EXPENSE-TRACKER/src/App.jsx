c5e953bfe26a18e1d2c562238441db17105a8780
KUNAL CHOURE <chourekunal4@gmail.com>
2024-07-07 12:16:54 +0530

    updated

--- EXPENSE-TRACKER/src/App.jsx
+++ EXPENSE-TRACKER/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Tracker from './show'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-      
+      <h1>EXPENSE TRACKER</h1>
+      <Tracker/>
     </>
   )
 }
---
+++ EXPENSE-TRACKER/src/show.jsx
@@ -0,0 +1,17 @@
+import './show.css'
+export default function Tracker(){
+
+    return(
+        <>
+    <div className="main">
+        <div className="track">
+        <h1 >HELLO</h1>
+           <div className="amt">
+            <p>Total:</p>
+            <h2>&#8377;500</h2>
+           </div>
+        </div>
+        </div>
+        </>
+    )
+}

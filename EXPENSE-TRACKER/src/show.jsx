7fcf15b41e0623feb4b2824e8916d8e131091b6d
KUNAL CHOURE <chourekunal4@gmail.com>
2024-07-09 21:58:56 +0530

    updated

--- EXPENSE-TRACKER/src/show.jsx
+++ EXPENSE-TRACKER/src/show.jsx
@@ -4,18 +4,30 @@ import Forms from './form';
 
 export default function Tracker() {
     let [data,setdata]=useState({description: "",amount:null,category:""});
+    let [expense,setexpense]=useState([]);
     let handlechange=(event)=>{
         let feild =event.target.name;
         let val=event.target.value;
-       
+       console.log(val)
         setdata((currentval)=>{
             currentval[feild]=val;
             return{...currentval}
         });
     }
 
-    let handlesubmit=()=>{
-        return{...data}
+    let handlesubmit=(event)=>{
+        event.preventDefault();
+        setexpense([
+            ...expense,{
+                description:data.description,amount:data.amount,category:data.category
+            }
+        ]);
+        
+        setdata({
+            description:"",
+            amount:null,
+            category:""
+        });
     }
     return (
         <>
@@ -27,6 +39,13 @@ export default function Tracker() {
                         <h2>&#8377;500</h2>
                     </div>
                     <ul>
+                        {expense.map((expense,index)=>{
+                            <li key={index}>
+                                {expense.description}- {expense.amount}
+                                <br />
+                                {expense.category}
+                            </li>
+                        })}
                         <li></li>
                     </ul>
                 </div>

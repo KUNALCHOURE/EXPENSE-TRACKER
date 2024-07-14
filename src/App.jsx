b9c0bc4ce55a917d823ab1f2af7da92545d06fae
KUNAL CHOURE <chourekunal4@gmail.com>
2024-07-14 14:14:03 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Tracker from './show'
+
+function App() {
+ 
+  return (
+    <>
+      <h1>EXPENSE TRACKER</h1>
+      <Tracker/>
+    </>
+  )
+}
+
+export default App
